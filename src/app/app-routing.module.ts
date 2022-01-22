1c101b5bbd96c960af44be99393c37dab2a270f3
minhhoa2832 <anhnguyendinh2832@gmail.com>
2022-01-22 13:04:44 +0700

    tao dk

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
+import {RegisterComponent} from "./pages/register/register.component";
+import {HomeComponent} from "./pages/homepage/home.component";
+import {AdminAuthGuard} from "./helper/admin-auth-guard";
+import {AdminComponent} from "./pages/admin/admin.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'homepage',
+    component: HomeComponent
+  },
+  {
+    path:'admin',
+    canActivate: [AdminAuthGuard],
+    component: AdminComponent
+  },
+  // {
+  //   path: 'pages',
+  //   loadChildren: () => import('./pages/pages.module').then(module => module.PagesModule)
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
